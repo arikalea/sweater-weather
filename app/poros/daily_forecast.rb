fb6b0574f16a7e354881d302bd7a4c0fe65aa189
Arika Turing <ablenker@gmail.com>
2021-04-24 14:29:18 -0600

    Add daily forecast poro and test

---
+++ app/poros/daily_forecast.rb
@@ -0,0 +1,25 @@
+class DailyForecast
+  attr_reader :date,
+              :sunrise,
+              :sunset,
+              :max_temp,
+              :min_temp,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @date = data[:dt]
+    @sunrise = data[:sunrise]
+    @sunset = data[:sunset]
+    @max_temp = data[:temp][:max]
+    @min_temp = data[:temp][:min]
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
+  end
+
+  def next_five_days(data)
+    data.take(5).map do |daily_data|
+      DailyForecast.new(daily_data)
+    end 
+  end
+end
