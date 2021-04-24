1ee5030d80c507f0d9e65cdc64ebd3244f36cf68
Arika Turing <ablenker@gmail.com>
2021-04-24 17:19:27 -0600

    Add testing for fields that are not present

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -7,13 +7,14 @@ RSpec.describe 'Forecast request' do
         get '/api/v1/forecast?location=denver,co'
 
         forecast = JSON.parse(response.body, symbolize_names: true)
-
+        
         expect(response).to be_successful
         expect(response.status).to eq(200)
         expect(forecast).to have_key(:data)
         expect(forecast[:data].keys).to eq([:id, :type, :attributes])
         expect(forecast[:data][:attributes].keys).to eq([:location, :current, :hourly, :daily])
         expect(forecast[:data][:attributes][:current].keys).to eq([:datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon])
+        expect(forecast[:data][:attributes][:current].keys).to_not eq([:pressure, :dew_point, :clouds])
         expect(forecast[:data][:attributes][:hourly].first.keys).to eq([:time, :temperature, :conditions, :icon])
         expect(forecast[:data][:attributes][:daily].first.keys).to eq([:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon])
       end
