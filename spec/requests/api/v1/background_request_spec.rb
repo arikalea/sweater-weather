223a8696705b21a477293ffc2ce5d271d98be1f9
Arika Turing <ablenker@gmail.com>
2021-04-25 13:17:23 -0600

    Add testing for number of results

--- spec/requests/api/v1/background_request_spec.rb
+++ spec/requests/api/v1/background_request_spec.rb
@@ -7,11 +7,12 @@ RSpec.describe 'Background request' do
         get '/api/v1/backgrounds?location=denver,co'
 
         image = JSON.parse(response.body, symbolize_names: true)
-
+        
         expect(response).to be_successful
         expect(response.status).to eq(200)
         expect(image).to have_key(:data)
         expect(image[:data].keys).to eq([:id, :type, :attributes])
+        expect(image[:data][:attributes].count).to eq(3)
         expect(image[:data][:attributes].keys).to eq([:location, :url, :credit])
         expect(image[:data][:attributes][:credit].keys).to eq([:source, :photographer])
       end
