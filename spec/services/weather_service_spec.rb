2a4604e1e9a3f979c894a6e93a59c7ccbe4521a0
Arika Turing <ablenker@gmail.com>
2021-04-25 13:12:39 -0600

    Add weather service test

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Weather Service' do
+  it 'can retrieve forecast based on location' do
+    VCR.use_cassette('denver_forecast') do
+      location_data = GeoService.location_data('Denver')
+      location = Location.new(location_data)
+      forecast_results = WeatherService.get_forecast_by_coords(location.coordinates)
+
+      expect(forecast_results).to be_a(Hash)
+      expect(forecast_results).to have_key(:current)
+      expect(forecast_results).to have_key(:hourly)
+      expect(forecast_results).to have_key(:daily)
+    end
+  end
+end
