f61957d6e6725994ebc91772ad7b98e0c294d71b
Arika Turing <ablenker@gmail.com>
2021-04-25 12:49:17 -0600

    Take out variable

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ForecastsController < ApplicationController
   def show
     location = params[:location]
     forecast_data = ForecastFacade.get_forecast(location)
-    # render json: forecast_data
-    data = render json: ForecastSerializer.new(forecast_data)
+    render json: ForecastSerializer.new(forecast_data)
   end
 end
