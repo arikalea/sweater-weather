bba582099237b271bdeca216ed13abfce9ab0483
Arika Turing <ablenker@gmail.com>
2021-04-26 11:34:42 -0600

    Add attributes to test

--- spec/requests/api/v1/teleport_request_spec.rb
+++ spec/requests/api/v1/teleport_request_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe 'Teleport request' do
       expect(salaries).to have_key(:data)
       expect(salaries[:data].keys).to eq([:id, :type, :attributes])
       expect(salaries[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
+      expect(salaries[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+      expect(salaries[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
     end
   end
 end
