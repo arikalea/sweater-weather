7a167f0a3a2de676266eaf00f9d46889cf418e43
Arika Turing <ablenker@gmail.com>
2021-04-26 16:42:32 -0600

    Change geoservice to mapquestservice

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::RoadTripsController < ApplicationController
   def create
-    
+    origin = road_trip_params[:origin]
+    destination = road_trip_params[:destination]
+    user = User.find_by(api_key: road_trip_params[:api_key])
+    if user
+      road_trip = RoadTripFacade.get_road_trip(origin, destination, user)
+    end 
+  end
+
+  private
+
+  def road_trip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
