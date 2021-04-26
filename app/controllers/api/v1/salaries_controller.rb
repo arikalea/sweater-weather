b5150bed7cee635e9004c45992213cfe80a90ae2
Arika Turing <ablenker@gmail.com>
2021-04-26 11:01:58 -0600

    Collect specific jobs into array

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -10,8 +10,17 @@ class Api::V1::SalariesController < ApplicationController
     end
 
     tele_parse = JSON.parse(tele_response.body, symbolize_names: true)
-    require "pry";binding.pry
 
-    
+    jobs = []
+    tele_parse[:salaries].map do |job|
+      jobs << job if job[:job][:title] == "Data Analyst"
+      jobs << job if job[:job][:title] == "Data Scientist"
+      jobs << job if job[:job][:title] == "Mobile Developer"
+      jobs << job if job[:job][:title] == "QA Engineer"
+      jobs << job if job[:job][:title] == "Software Engineer"
+      jobs << job if job[:job][:title] == "Systems Administrator"
+      jobs << job if job[:job][:title] == "Web Developer"
+    end
+    require "pry";binding.pry
   end
 end
