99da1950de3ed399acad37ce1fdb03b75679e1e5
Arika Turing <ablenker@gmail.com>
2021-04-26 10:25:44 -0600

    Cant get tele response to work

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::SalariesController < ApplicationController
   def show
-    destination = params['destination']
+    ua_id = params['destination']
 
     tele_conn = Faraday.new(url: 'https://api.teleport.org/api')
-    #
-    tele_response = tele_conn.get("/urban_areas/{ua_id}/salaries/") do |f|
-      f.params['slug'] = destination
+
+    tele_response = tele_conn.get("/urban_areas/slug:#{ua_id}/") do |f|
+    # tele_response = tele_conn.get("/urban_areas/slug:#{ua_id}/salaries/") do |f|
+      # f.params['slug'] = ua_id
     end
 
     tele_parse = JSON.parse(tele_response.body, symbolize_names: true)
