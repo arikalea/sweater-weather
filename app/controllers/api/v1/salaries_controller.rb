3c74c00190b0426d6f251eff74d67f75b9c2f7a7
Arika Turing <ablenker@gmail.com>
2021-04-26 11:09:50 -0600

    Access forcast info in controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -21,6 +21,9 @@ class Api::V1::SalariesController < ApplicationController
       jobs << job if job[:job][:title] == "Systems Administrator"
       jobs << job if job[:job][:title] == "Web Developer"
     end
+    forecast_data = ForecastFacade.get_forecast(ua_id)
+    forecast_summary = forecast_data.current.conditions
+    forecast_temp = forecast_data.current.temperature
     require "pry";binding.pry
   end
 end
