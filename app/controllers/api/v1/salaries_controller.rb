3ac0299664dd80d0a95fb6887282c04ad93d57f1
Arika Turing <ablenker@gmail.com>
2021-04-26 09:58:09 -0600

    Still working through response in controller

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::SalariesController < ApplicationController
   def show
-    # search = params[:search]
+    destination = params['destination']
+
+    tele_conn = Faraday.new(url: 'https://api.teleport.org/api')
     #
-    # tele_conn = Faraday.new(url: 'https://api.teleport.org/api/')
-    #
-    # tele_response = tele_conn.get('/api/cities/') do |f|
-    #   f.params['search'] = search
-    #   f.params['limit'] = 1
-    # end
-    # tele_parse = JSON.parse(tele_response.body)['_embedded']['city:search-results'][0]['_links']['city:item']['href']
+    tele_response = tele_conn.get("/urban_areas/{ua_id}/salaries/") do |f|
+      f.params['slug'] = destination
+    end
+
+    tele_parse = JSON.parse(tele_response.body, symbolize_names: true)
+    require "pry";binding.pry
     #
     # response = Faraday.get(tele_parse)
     # parse = JSON.parse(response.body, symbolize_names: true)
-    require "pry";binding.pry
   end
 end
