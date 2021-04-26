6d36c2843525923a6376d8660e8709bf99729b9f
Arika Turing <ablenker@gmail.com>
2021-04-26 10:41:57 -0600

    Finally got response to work

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -2,17 +2,16 @@ class Api::V1::SalariesController < ApplicationController
   def show
     ua_id = params['destination']
 
-    tele_conn = Faraday.new(url: 'https://api.teleport.org/api')
+    tele_conn = Faraday.new(url: 'https://api.teleport.org/api/') do |f|
+    end
 
-    tele_response = tele_conn.get("/urban_areas/slug:#{ua_id}/") do |f|
-    # tele_response = tele_conn.get("/urban_areas/slug:#{ua_id}/salaries/") do |f|
+    tele_response = tele_conn.get("urban_areas/slug:#{ua_id}/salaries/") do |f|
       # f.params['slug'] = ua_id
     end
 
     tele_parse = JSON.parse(tele_response.body, symbolize_names: true)
     require "pry";binding.pry
-    #
-    # response = Faraday.get(tele_parse)
-    # parse = JSON.parse(response.body, symbolize_names: true)
+
+    
   end
 end
