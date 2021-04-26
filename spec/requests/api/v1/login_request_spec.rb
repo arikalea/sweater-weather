b57fbd7a0d6eff2c2a64fe20f51dacf0fdd8b1e3
Arika Turing <ablenker@gmail.com>
2021-04-26 14:01:53 -0600

    Add more expectations to login request spec

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -2,28 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'User login request' do
   describe 'happy path' do
-    before :each do
-      user_params = { email: 'example@email.com',
-                      password: 'password',
-                      password_confirmation: 'password'}
-
-      headers = {"CONTENT_TYPE" => "application/json",
-                 "ACCEPT" => "application/json"}
-
-      post '/api/v1/users', headers: headers, params: user_params.to_json
-    end
-    
     it 'can login a user' do
-      user_params = { email: 'example@email.com',
-                      password: 'password' }
+      user = create(:user)
+      login_params = { email: user.email,
+                       password: user.password }
 
       headers = {"CONTENT_TYPE" => "application/json",
                  "ACCEPT" => "application/json"}
 
-      post '/api/v1/sessions', headers: headers, params: user_params.to_json
+      post '/api/v1/sessions', headers: headers, params: login_params.to_json
 
+      login_json = JSON.parse(response.body, symbolize_names: true)
+      
       expect(response).to be_successful
       expect(response.status).to eq(200)
+      expect(response.content_type).to eq("application/json")
+      expect(login_json).to have_key(:data)
+      expect(login_json[:data].keys).to eq([:id, :type, :attributes])
+      expect(login_json[:data][:attributes].count).to eq(2)
+      expect(login_json[:data][:type]).to eq('user')
+      expect(login_json[:data][:attributes][:email]).to eq(user.email)
+      expect(login_json[:data][:attributes][:api_key]).to eq(user.api_key)
     end
   end
 end
