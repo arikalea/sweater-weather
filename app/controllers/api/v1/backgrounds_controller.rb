634ac64a8af6e92aea974f4e3642aca075002c16
Arika Turing <ablenker@gmail.com>
2021-04-27 15:13:09 -0600

    Add sad path to background spec

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::BackgroundsController < ApplicationController
+  before_action :validate_params
+
   def show
     location = params[:location]
     background_data = BackgroundFacade.get_background(location)
     render json: ImageSerializer.new(background_data)
+  end
+
+  private
+
+  def validate_params
+    if params[:location].blank?
+      render json: { error: 'Your parameters are bad' }, status: :bad_request
+    end
   end
 end
