707f6772b30c1c3eaa526af2c29a6621846f0532
Arika Turing <ablenker@gmail.com>
2021-04-27 13:26:47 -0600

    Finish happy path for roadtrip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,6 +1,9 @@
 class RoadTripFacade
-  def self.get_road_trip(origin, destination, user)
+  def self.get_road_trip(origin, destination)
     road_trip_data = MapQuestService.road_trip_data(origin, destination)
-    RoadTrip.new(road_trip_data)
+    destination_data = MapQuestService.location_data(destination)
+    destination_coords = destination_data[:results][0][:locations][0][:latLng]
+    forecast_data = WeatherService.get_forecast_by_coords(destination_coords)
+    RoadTrip.new(road_trip_data, origin, destination, forecast_data)
   end
 end
---
+++ spec/facades/road_trip_facade_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Facade' do
+  it '.get_road_trip' do
+    VCR.use_cassette('reno_road_trip') do
+      origin = "Denver"
+      destination = "Reno"
+      # road_trip = MapQuestService.road_trip_data(origin, destination)
+      facade = RoadTripFacade.get_road_trip(origin, destination)
+      
+      expect(facade).to be_a(RoadTrip)
+      expect(facade.end_city).to be_a(String)
+      expect(facade.end_city).to eq(destination)
+      expect(facade.start_city).to be_a(String)
+      expect(facade.start_city).to eq(origin)
+      expect(facade.travel_time).to be_a(String)
+      expect(facade.weather_at_eta).to be_a(Hash)
+      expect(facade.weather_at_eta.keys).to eq([:temperature, :conditions])
+      expect(facade.weather_at_eta[:temperature]).to be_a(Float)
+      expect(facade.weather_at_eta[:conditions]).to be_a(String)
+    end
+  end
+end
