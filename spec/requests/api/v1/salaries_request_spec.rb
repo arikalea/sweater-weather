8f29aa7811181e4186f42c540efc2ff76215fdad
Arika Turing <ablenker@gmail.com>
2021-04-27 10:01:55 -0600

    Adjust salaries request spec for serializer

---
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Salaries request' do
+  describe 'It can retrieve salary and forecast info for a city' do
+    it 'has successful response' do
+      get '/api/v1/salaries?destination=chicago'
+
+      salaries = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(salaries).to have_key(:data)
+      expect(salaries[:data]).to be_a(Hash)
+      expect(salaries[:data].keys).to eq([:id, :type, :attributes])
+      expect(salaries[:data][:id]).to eq('null')
+      expect(salaries[:data][:type]).to eq('salaries')
+      expect(salaries[:data][:attributes].keys).to eq([:destination, :forecast, :salaries])
+      expect(salaries[:data][:attributes][:forecast].keys).to eq([:summary, :temperature])
+      expect(salaries[:data][:attributes][:salaries]).to be_an(Array)
+      expect(salaries[:data][:attributes][:salaries].first.keys).to eq([:title, :min, :max])
+    end
+  end
+end
