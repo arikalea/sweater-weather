580f72c50e5d90c163d332622a81d26c13e09c28
Arika Turing <ablenker@gmail.com>
2021-04-27 15:01:39 -0600

    Change to ImageSerializer

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::BackgroundsController < ApplicationController
   def show
     location = params[:location]
     background_data = BackgroundFacade.get_background(location)
-    render json: BackgroundSerializer.new(background_data)
+    render json: ImageSerializer.new(background_data)
   end
 end
