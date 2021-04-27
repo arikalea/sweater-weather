3609be09082d3a4b55cf44541af6d5f0830b79c4
Arika Turing <ablenker@gmail.com>
2021-04-27 09:23:18 -0600

    Set up salaries service spec to create job poro

--- app/services/salaries_service.rb
+++ app/services/salaries_service.rb
@@ -1,8 +1,8 @@
 class SalariesService
-  def self.get_salary_info(ua_id)
+  def self.get_salary_info(destination)
     # tele_conn = Faraday.new(url: 'https://api.teleport.org/api/')
     # tele_parse = JSON.parse(tele_response.body, symbolize_names: true)
-    tele_response = conn.get("urban_areas/slug:#{ua_id}/salaries/")
+    tele_response = conn.get("urban_areas/slug:#{destination}/salaries/")
     parse(tele_response)
   end
 
---
+++ spec/services/salaries_service_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe SalariesService do
+  xit 'returns salaries for specific jobs' do
+    jobs = SalariesService.get_salary_info('denver')
+
+    expect(jobs).to be_an(Array)
+    expect(jobs.count).to eq(7)
+    expect(jobs.first).to be_a(Job)
+  end
+
+  it 'finds specific jobs' do
+    job_1 = Job.new({
+            "job": {
+                "id": "ARCHITECT",
+                "title": "Architect"
+            },
+            "salary_percentiles": {
+                "percentile_25": 43470.96334773236,
+                "percentile_50": 53828.81526137848,
+                "percentile_75": 66654.63862085681
+            }
+        })
+    job_2 = Job.new({
+            "job": {
+                "id": "DATA-ANALYST",
+                "title": "Data Analyst"
+            },
+            "salary_percentiles": {
+                "percentile_25": 42878.34161807408,
+                "percentile_50": 51604.570316663645,
+                "percentile_75": 62106.68549841864
+            }
+        })
+    all_jobs = [job_1, job_2]
+    result = SalariesService.find_job_titles(all_jobs)
+
+    expect(result).to be_an(Array)
+    expect(result.count).to eq(1)
+    expect(result).to include(job_2)
+    expect(result).to_not include(job_1)
+  end
+end
