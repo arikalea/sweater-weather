a0214a3e70390702db68cd3f2245252a2c2a4b36
Arika Turing <ablenker@gmail.com>
2021-04-27 17:17:14 -0600

    Add expectations to roadtrip request spec

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::RoadTripsController < ApplicationController
     origin = road_trip_params[:origin]
     destination = road_trip_params[:destination]
     road_trip = RoadTripFacade.get_road_trip(origin, destination)
-    render json: RoadTripSerializer.new(road_trip), status: 201
+    render json: RoadTripSerializer.new(road_trip)
   end
 
   private
@@ -16,9 +16,9 @@ class Api::V1::RoadTripsController < ApplicationController
     else
       params = JSON.parse(request.body.read, symbolize_names: true)
       if params[:origin].blank? || params[:destination].blank?
-        render json: { error: user.errors.full_messages.to_sentence }, status: :bad_request
+        render json: { error: 'Must provide request body' }, status: :bad_request
       elsif !User.find_by(api_key: road_trip_params[:api_key])
-        render json: { error: user.errors.full_messages.to_sentence }, status: :unauthorized
+        render json: { error: 'Must provide valid API key' }, status: :unauthorized
       end
     end
   end
