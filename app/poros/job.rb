dbc637f5a973e8a8ad6e7505d815565f4fd330cf
Arika Turing <ablenker@gmail.com>
2021-04-27 09:48:32 -0600

    Change data type of min and max

--- app/poros/job.rb
+++ app/poros/job.rb
@@ -5,7 +5,7 @@ class Job
 
   def initialize(data)
     @title = data[:job][:title]
-    @min = data[:salary_percentiles][:percentile_25].round(2)
-    @max = data[:salary_percentiles][:percentile_75].round(2)
+    @min = "$#{data[:salary_percentiles][:percentile_25].round(2)}"
+    @max = "$#{data[:salary_percentiles][:percentile_75].round(2)}"
   end
 end
