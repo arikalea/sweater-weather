0d6949b07a2419149659a288c44c3007968817d3
Arika Turing <ablenker@gmail.com>
2021-04-27 09:39:11 -0600

    Add job poro and pass spec

--- spec/poros/job_poro_spec.rb
+++ spec/poros/job_poro_spec.rb
@@ -3,15 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Job poro' do
   describe 'happy path' do
     it 'can create poro' do
-      attrs = { title: 'Data Analyst',
-                min: 42878.34161807408,
-                max: 62106.68549841864 }
+      attrs = {
+              job: {
+                  id: "DATA-ANALYST",
+                  title: "Data Analyst"
+              },
+              salary_percentiles: {
+                  percentile_25: 42878.34161807408,
+                  percentile_50: 51604.570316663645,
+                  percentile_75: 62106.68549841864
+              }
+          }
       job = Job.new(attrs)
-
+      
       expect(job).to be_a(Job)
-      expect(job.title).to eq(attrs[:title])
-      expect(job.min).to eq(attrs[:min].round(2))
-      expect(job.max).to eq(attrs[:max].round(2))
+      expect(job.title).to eq(attrs[:job][:title])
+      expect(job.min).to eq(attrs[:salary_percentiles][:percentile_25].round(2))
+      expect(job.max).to eq(attrs[:salary_percentiles][:percentile_75].round(2))
     end
   end
 end
