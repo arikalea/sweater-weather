cb35945cf651db290bd99b120a1935fb632e6645
Arika Turing <ablenker@gmail.com>
2021-04-27 09:53:11 -0600

    Add expectaions for salaries attribute

--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Salaries Facade' do
+RSpec.describe SalariesFacade do
   it 'can be created with all attributes' do
     salaries_facade = SalariesFacade.new('denver')
     # require "pry";binding.pry
@@ -8,6 +8,7 @@ RSpec.describe 'Salaries Facade' do
     expect(salaries_facade.destination).to eq('denver')
     expect(salaries_facade.forecast).to be_a(Hash)
     expect(salaries_facade.forecast.keys).to eq([:summary, :temperature])
-    # expect(salaries_facade.salaries).to eq()
+    expect(salaries_facade.salaries).to be_an(Array)
+    expect(salaries_facade.salaries.first).to be_a(Job)
   end
 end
